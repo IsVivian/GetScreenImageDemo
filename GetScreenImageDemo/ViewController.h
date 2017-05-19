46814e700ffb76800396bdf7d2528853c8a81338
isVivian <384120230@qq.com>
2017-05-19 13:29:40 +0800

    Initial Commit

---
+++ GetScreenImageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetScreenImageDemo
+//
+//  Created by iOS on 2017/5/19.
+//  Copyright © 2017年 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
